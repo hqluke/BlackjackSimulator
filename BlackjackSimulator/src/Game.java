fb85980ac49af8e88906bab1032ea8bc89f124de
LukePeterson1 <lukepeterson1@mail.weber.edu>
2025-10-08 06:34:43 -0600

    Added notes & game pseudo code

--- BlackjackSimulator/src/Game.java
+++ BlackjackSimulator/src/Game.java
@@ -3,10 +3,24 @@ public class Game {
     private Deck deck;
     private Player player;
     private Dealer dealer;
+    private GUI gui;
     private int numDecks;
     private double minimumBet;
     
-    //WIP this will be the class that handles every other class and allows the game to actually be played
-    //need to add gui in future
-    //change error messages to pop ups later
+    
+    //Select number of decks
+    
+    //Select starting money amount
+    
+    //Select min bet	
+    
+    //Set up deck
+    
+    //while(player.money >= 0){
+//    Give the first card to the player hand list, then dealer hand list, then player,
+    //then dealer (passing in hide card). Then display the cards using the GUI/animations
+//    -  Then allow the player to make their plays. Then have the dealer reveal their cards.
+//    - Determine winner
+//    - Payout bets if needed
+    //}
 }
