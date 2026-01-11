563ad06082e8abd13b1092635bc924ffe3952827
hqluke <212039062+hqluke@users.noreply.github.com>
2026-01-10 22:44:00 -0700

    fixed windows install

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal enabledelayedexpansion
+
 REM Blackjack Simulator Launcher Script
 
 cd /d "%~dp0"
@@ -16,45 +18,75 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-REM Auto-detect and set JAVA_HOME if not set
-if "%JAVA_HOME%"=="" (
-    echo Detecting JAVA_HOME...
-    
-    REM Try common Java installation locations
-    if exist "C:\Program Files\Eclipse Adoptium\jdk-21*" (
-        for /d %%i in ("C:\Program Files\Eclipse Adoptium\jdk-21*") do set "JAVA_HOME=%%i"
-    ) else if exist "C:\Program Files\Java\jdk-21*" (
-        for /d %%i in ("C:\Program Files\Java\jdk-21*") do set "JAVA_HOME=%%i"
-    ) else if exist "C:\Program Files\OpenJDK\jdk-21*" (
-        for /d %%i in ("C:\Program Files\OpenJDK\jdk-21*") do set "JAVA_HOME=%%i"
-    ) else if exist "C:\Program Files\Eclipse Adoptium\jdk-25*" (
-        for /d %%i in ("C:\Program Files\Eclipse Adoptium\jdk-25*") do set "JAVA_HOME=%%i"
-    ) else if exist "C:\Program Files\Java\jdk-25*" (
-        for /d %%i in ("C:\Program Files\Java\jdk-25*") do set "JAVA_HOME=%%i"
-    )
-    
-    if not "%JAVA_HOME%"=="" (
-        echo Using Java from: %JAVA_HOME%
-    ) else (
-        echo WARNING: Could not auto-detect JAVA_HOME
-        echo Please set JAVA_HOME environment variable
-    )
-)
+REM Display Java version
+echo Java version:
+java -version 2>&1
+echo.
 
-REM Check Java version
-for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -version 2^>^&1') do set "JAVA_VERSION=%%j"
-if %JAVA_VERSION% LSS 21 (
-    echo ERROR: Java 21 or higher is required!
-    echo Current version: 
-    java -version 2>&1
+REM Set JAVA_HOME if not already set
+if "%JAVA_HOME%"=="" (
+    echo JAVA_HOME is not set. Attempting to detect...
+    
+    REM Use java to tell us where it's installed
+    for /f "tokens=*" %%i in ('java -XshowSettings:properties -version 2^>^&1 ^| findstr "java.home"') do (
+        set "JAVA_LINE=%%i"
+    )
+    
+    REM Extract the path from "java.home = <path>"
+    if defined JAVA_LINE (
+        for /f "tokens=2* delims==" %%a in ("!JAVA_LINE!") do (
+            set "JAVA_HOME=%%a"
+            REM Trim leading spaces
+            for /f "tokens=* delims= " %%b in ("!JAVA_HOME!") do set "JAVA_HOME=%%b"
+        )
+    )
+    
+    REM Fallback: Search common installation directories
+    if not defined JAVA_HOME (
+        echo Searching common Java installation directories...
+        for /d %%d in ("C:\Program Files\Java\jdk*") do (
+            if exist "%%d\bin\java.exe" (
+                set "JAVA_HOME=%%d"
+                echo Found Java at: !JAVA_HOME!
+                goto :home_found
+            )
+        )
+        for /d %%d in ("C:\Program Files\Eclipse Adoptium\jdk*") do (
+            if exist "%%d\bin\java.exe" (
+                set "JAVA_HOME=%%d"
+                echo Found Java at: !JAVA_HOME!
+                goto :home_found
+            )
+        )
+        for /d %%d in ("C:\Program Files\Oracle\jdk*") do (
+            if exist "%%d\bin\java.exe" (
+                set "JAVA_HOME=%%d"
+                echo Found Java at: !JAVA_HOME!
+                goto :home_found
+            )
+        )
+    )
+    
+    :home_found
+    if defined JAVA_HOME (
+        echo Detected JAVA_HOME: !JAVA_HOME!
+        REM Verify it's valid
+        if not exist "!JAVA_HOME!\bin\java.exe" (
+            echo WARNING: Detected JAVA_HOME does not contain bin\java.exe
+            set "JAVA_HOME="
+        )
+    )
+    
+    if not defined JAVA_HOME (
+        echo WARNING: Could not detect JAVA_HOME
+        echo Maven will attempt to use java from your PATH
+    )
     echo.
-    echo Please download from: https://adoptium.net/
-    echo.
-    pause
-    exit /b 1
 )
 
 REM Run the game
+echo Starting game...
+echo.
 call mvnw.cmd clean javafx:run
 
 REM Keep window open if there's an error
@@ -63,3 +95,5 @@ if %errorlevel% neq 0 (
     echo Error running the game. See above for details.
     pause
 )
+
+endlocal
